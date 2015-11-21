2c2a581266748be18d4ac5aa8b9d8d2258388235
Mrfuzzy <league@iMac.attlocal.net>
2015-11-20 18:22:20 -0800

    11/20

--- workspace/Level1/src/WhackAMole.java
+++ workspace/Level1/src/WhackAMole.java
@@ -1,20 +1,27 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class WhackAMole implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	Date timeAtStart = new Date();
+	int misses = 0;
+	int moles = 0;
 
 	public static void main(String[] args) {
 		new WhackAMole().createUI();
 	}
 
 	void createUI() {
+		frame = new JFrame();
+		panel = new JPanel();
 		frame.setSize(300, 300);
 		frame.add(panel);
 		drawButtons(24);
@@ -44,17 +51,35 @@ public class WhackAMole implements ActionListener {
 			e.printStackTrace();
 		}
 	}
+	
+	private long time() {
+		Date timeAtEnd = new Date();
+		long gameDuration = timeAtEnd.getTime() - timeAtStart.getTime();
+		long gameInSeconds = (gameDuration / 1000) % 60;
+		return gameInSeconds;
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String chosenText = ((JButton) e.getSource()).getText();
 		if (chosenText.equals("mole!")) {
 			System.out.println("mole!");
+			moles++;
 			frame.dispose();
 			createUI();
+			if (moles == 10) {
+				JOptionPane.showMessageDialog(null, "it took you "+time()+" seconds to hit 10 moles");
+				System.exit(0);
+			}
 		} else {
 			System.out.println("miss");
-			speak("miss");
+			misses++;
+			if (misses == 5) {
+				frame.dispose();
+				createUI();
+				misses = 0;
+			}
+
 		}
 
 	}
